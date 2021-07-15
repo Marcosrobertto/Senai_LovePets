f6a9ddaab03f407afaa8ad7096a45203c4bd2ebb
Marcos Roberto Santos Brito <48784309822@info.local>
2021-07-15 17:11:05 -0300

    Projeto Adicionado

---
+++ BackEnd/senai_lovePets_webApi/senai_lovePets_webApi/Interfaces/IClinicaRepository.cs
@@ -0,0 +1,27 @@
+﻿using senai_lovePets_webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_lovePets_webApi.Interfaces
+{
+    interface IClinicaRepository
+    {
+
+        List<Clinica> Listar();
+
+        
+        Clinica BuscarPorId(int id);
+
+        
+        void Cadastrar(Clinica novaClinica);
+
+        
+        void Atualizar(int id, Clinica instituicaoAtualizada);
+
+        
+        void Deletar(int id);
+
+    }
+}
